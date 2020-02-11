4f52d950b43d3072b121cc17a0eb9b054dbcc017
karlexceed <alex.decker@gmail.com>
2020-02-10 22:32:46 -0600

    Initial commit!

---
+++ init.lua
@@ -0,0 +1,12 @@
+minetest.register_globalstep(function(dtime)
+	for _, player in ipairs(minetest.get_connected_players()) do
+		local pos = vector.new(player:get_pos())
+		local pos1 = vector.subtract(pos, { x = 10, y = 10, z = 10 })
+		local pos2 = vector.add(pos, { x = 10, y = 10, z = 10 })
+		
+		for _, tmp_node in ipairs(minetest.find_nodes_in_area(pos1, pos2, { "group:falling_node" })) do
+			minetest.punch_node(tmp_node)
+		end
+	end
+end)
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# engage_gravity
+### by karlexceed
+
+A very simple mod that makes nodes in the falling_nodes group begin to fall when a player gets close.
+
+### Dependencies
+None!
+
+### Software Requirements
+This mod has only been tested with Minetest 5.2.0, but should work with earlier versions.
+
